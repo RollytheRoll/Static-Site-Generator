7c78764335656c989a03a4316f46371bbfb146ef
RollytheRoll <pillerkurt1995@gmail.com>
2025-03-15 21:47:18 +0100

    textnode to htmlnode fertig, markdown to textnode fertig gestellt

--- src/main.py
+++ src/main.py
@@ -6,18 +6,5 @@ def main():
     text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(text_node)
 
-def text_node_to_html_node(text_node):
-        match text_node:
-            case TextType.TEXT:
-                return LeafNode(None, text_node.text)
-            case TextType.BOLD:
-                return LeafNode("b", text_node.text)
-            case TextType.ITALIC:
-                return LeafNode("i", text_node.text)
-            case TextType.CODE:
-                return LeafNode("code", text_node.text)
-            case TextType.LINK:
-                return LeafNode("a", text_node.text, text_node.url)
-            case TextType.IMAGE:
-                return LeafNode("img", "", {"src":text_node.url, "alt":text_node.text})
+
 main() 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,7 +1,7 @@
 import unittest
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from main import text_node_to_html_node
-from textnode import *
+from textnode import TextType, TextNode, text_node_to_html_node
+from markdown  import split_nodes_delimiter
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -119,6 +119,40 @@ class TestLeafNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold node")
+
+    def test_italic(self):
+        node = TextNode("This is a italic node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a italic node")
+
+    def test_code(self):
+        node = TextNode("This is code", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is code")
+
+    def test_link(self):
+        node = TextNode("This is a link", TextType.LINK, {"href":"www.boot.dev"})
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link")
+    
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
 
 if __name__ == "__main__":
     unittest.main()
