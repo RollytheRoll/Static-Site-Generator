75d8fae986980640231fb3eb12469b36b74138cd
RollytheRoll <pillerkurt1995@gmail.com>
2025-03-21 08:24:07 +0100

    settings für github page erstellt

--- src/main.py
+++ src/main.py
@@ -1,22 +1,28 @@
 import os
 import shutil
+import sys
 from markdown_blocks import *
 from htmlnode import *
 from markdown import *
 from textnode import *
 
 def main():
-    if os.path.exists("./public/"):
-        shutil.rmtree("./public/")
-    os.mkdir("./public")
+    if os.path.exists("./docs"):
+        shutil.rmtree("./docs")
+    os.mkdir("./docs")
     paths = os.listdir("./src/static")
     parent_path = "./src/static"
-    dst_path = "./public"
+    dst_path = "./docs"
     copy_static_to_public(paths, parent_path, dst_path)
+    if not sys.argv[0] == "":
+        basepath = sys.argv[0]
+    else:
+        basepath = "/"
+
     from_path = "./content"
     template_path = "./template.html"
-    dest_path = "./public"
-    generate_page_recursive(from_path, template_path, dest_path)
+    dest_path = "./docs"
+    generate_page_recursive(from_path, template_path, dest_path, basepath)
 
 
 def copy_static_to_public(paths, parent_path, dst_path):
@@ -39,7 +45,7 @@ def extract_title(markdown):
         if line.startswith("#"):
             return line.lstrip("#").strip()
         
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     dest_path, _ = os.path.splitext(dest_path)
     dest_path = dest_path + ".html"
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
@@ -53,6 +59,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_file)
     template_file = template_file.replace("{{ Title }}", title)
     template_file = template_file.replace("{{ Content }}", content)
+    template_file = template_file.replace("href=\"/", f"href=\"{basepath}")
+    template_file = template_file.replace("src=\"/", f"src=\"{basepath}")
     if not os.path.exists(os.path.dirname(dest_path)):
         os.mkdir(os.path.dirname(dest_path))
     
@@ -60,18 +68,18 @@ def generate_page(from_path, template_path, dest_path):
     html.write(template_file)
     html.close()
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     paths = dir_path_content
     if os.path.isdir(paths):
         paths = os.listdir(paths)
     for path in paths:
         new_path = os.path.join(dir_path_content,path)
         if os.path.isfile(new_path):
-            generate_page(new_path,template_path,os.path.join(dest_dir_path,path))
+            generate_page(new_path,template_path,os.path.join(dest_dir_path,path),basepath)
             continue
         if os.path.isdir(new_path):
             os.mkdir(os.path.join(dest_dir_path, path))
-            generate_page_recursive(new_path,template_path,os.path.join(dest_dir_path, path))
+            generate_page_recursive(new_path,template_path,os.path.join(dest_dir_path, path), basepath)
 
 
 main() 
